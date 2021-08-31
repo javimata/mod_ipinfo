1726ca222c49119422a13a55d6dfcaf2a8f79166
Javi Mata <javimata@gmail.com>
2021-08-31 12:30:16 -0500

    change echos

--- mod_ipinfo.php
+++ mod_ipinfo.php
@@ -20,29 +20,17 @@ if ( $ip_address ) {
     $client = new IPinfo();
     $details = $client->getDetails($ip_address);
 
-    $info = [
-        "ip" => $details->ip,
-        "hostname" => $details->hostname,
-        "city" => $details->city,
-        "region" => $details->region,
-        "country" => $details->country,
-        "loc" => $details->loc,
-        "org" => $details->org,
-        "postal" => $details->postal,
-        "timezone" => $details->timezone
-    ];
-
     ?>
     <div class="ipinfo">
-        IP: <?php echo $info["ip"]; ?><br>
-        Hostname: <?php echo $info["hostname"]; ?><br>
-        City: <?php echo $info["city"]; ?><br>
-        Region: <?php echo $info["region"]; ?><br>
-        Country: <?php echo $info["country"]; ?><br>
-        Loc: <?php echo $info["loc"]; ?><br>
-        Org: <?php echo $info["org"]; ?><br>
-        Postal: <?php echo $info["postal"]; ?><br>
-        Timezone: <?php echo $info["timezone"]; ?>
+        IP: <?php echo $details->ip; ?><br>
+        Hostname: <?php echo $details->hostname; ?><br>
+        City: <?php echo $details->city; ?><br>
+        Region: <?php echo $details->region; ?><br>
+        Country: <?php echo $details->country; ?><br>
+        Loc: <?php echo $details->loc; ?><br>
+        Org: <?php echo $details->org; ?><br>
+        Postal: <?php echo $details->postal; ?><br>
+        Timezone: <?php echo $details->timezone; ?>
     </div>
 
     <?php
